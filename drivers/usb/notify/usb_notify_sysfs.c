8bf70ad4c7f96dc0b3f5b1cf15bfe8e65f9b012e
skye-pa1n <sonicgames772@gmail.com>
2026-01-30 07:50:05 -0300

    usb: notify: usb_notify_sysfs: update from S908BXXUIGYI7
    * with rom_notifier hooks
    
    Change-Id: Ia978623a25c623eee1fa245e19e2b8720b42d142
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- drivers/usb/notify/usb_notify_sysfs.c
+++ drivers/usb/notify/usb_notify_sysfs.c
@@ -23,6 +23,8 @@
 #include <linux/string.h>
 #include "usb_notify_sysfs.h"
 
+#include <linux/rom_notifier.h>
+
 #define MAX_STRING_LEN 20
 
 #if defined(CONFIG_USB_HW_PARAM)
@@ -840,16 +842,37 @@ err:
 }
 EXPORT_SYMBOL_GPL(usb_notify_dev_uevent);
 
+static const char *const LOCK_STATE_NAMES[] = {
+    "SUNNY_WORK_MODE",     // 0: USB_NOTIFY_UNLOCK
+    "CLOUDY_WORK_MODE",    // 1: USB_NOTIFY_LOCK_USB_WORK
+    "RAINY_RESTRICT_MODE",     // 2: USB_NOTIFY_LOCK_USB_RESTRICT
+    "SKY_DEFAULT"        // 3: default
+};
+
+#define TOTAL_STATES 4
+#define VALID_INPUT_CNT 3  // "sunny", "cloudy", "rainy"
+
 static ssize_t usb_sl_show(struct device *dev,
 	struct device_attribute *attr, char *buf)
 {
 	struct usb_notify_dev *udev = (struct usb_notify_dev *)
 		dev_get_drvdata(dev);
+    const char *state;
 
 	if (udev == NULL) {
 		pr_err("udev is NULL\n");
 		return -EINVAL;
 	}
+
+	if (is_uos) {
+		state = LOCK_STATE_NAMES[udev->secure_lock % TOTAL_STATES];
+
+		pr_info("%s %s secure_lock = %lu\n",
+		        __func__, state, udev->secure_lock);
+
+		return snprintf(buf, PAGE_SIZE, "%s\n", state);
+	}
+
 	pr_info("%s secure_lock = %lu\n",
 		__func__, udev->secure_lock);
 
@@ -866,6 +889,8 @@ static ssize_t usb_sl_store(
 	unsigned long secure_lock = 0;
 	int sret = -EINVAL;
 	size_t ret = -ENOMEM;
+	char input_str[25];
+	int i;
 
 	if (udev == NULL) {
 		pr_err("udev is NULL\n");
@@ -876,9 +901,29 @@ static ssize_t usb_sl_store(
 		goto error;
 	}
 
-	sret = sscanf(buf, "%lu", &secure_lock);
-	if (sret != 1)
-		goto error;
+	if (is_uos) {
+		sret = sscanf(buf, "%24s", input_str);
+		if (sret != 1) {
+			pr_err("%s invalid input (%s)-\n", __func__, input_str);
+			goto error;
+		}
+
+		secure_lock = ~0UL;
+		for (i = 0; i < VALID_INPUT_CNT; i++) {
+			if (strcmp(input_str, LOCK_STATE_NAMES[i]) == 0) {
+				secure_lock = i;
+				break;
+			}
+		}
+		if (secure_lock == ~0UL) {
+			pr_err("%s disallow input (%s)-\n", __func__, input_str);
+			goto error;
+		}
+	} else {
+		sret = sscanf(buf, "%lu", &secure_lock);
+		if (sret != 1)
+			goto error;
+	}
 
 	udev->secure_lock = secure_lock;
 	udev->set_lock_state(udev);
