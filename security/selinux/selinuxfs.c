eea7f7a095560f69d0ae0d7dbb979bae1652dfc1
//. <174224977+skye-tachyon@users.noreply.github.com>
2026-06-04 06:33:22 -0400

    security: selinux: add a permissive switch
    * for development
    * enable it with CONFIG_SECURITY_SELINUX_PERMISSIVE on your defconfig or on a .config fragment
    
    Change-Id: I6153c788c70b824cd273d433f2ad59713a66c692
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -158,7 +158,11 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
 
+#ifdef CONFIG_SECURITY_SELINUX_PERMISSIVE
+	new_value = 0;
+#else
 	new_value = !!new_value;
+#endif
 
 	old_value = enforcing_enabled(state);
 	if (new_value != old_value) {
