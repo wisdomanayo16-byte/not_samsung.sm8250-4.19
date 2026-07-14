62fa24a9c81f630c32aaf7b3063588754588be9d
Masami Hiramatsu <mhiramat@kernel.org>
2026-07-13 22:48:02 -0300

    UPSTREAM: kprobes: Initialize kprobes at postcore_initcall Initialize kprobes at postcore_initcall level instead of module_init since kprobes is not a module, and it depends on only subsystems initialized in core_initcall. This will allow ftrace kprobe event to add new events when it is initializing because ftrace kprobe event is initialized at later initcall level.
    Link: http://lkml.kernel.org/r/155851394736.15728.13626739508905120098.stgit@devnote2
    
    Change-Id: Ie4e676fe07a5b1d9de88831552a850d6b2460588
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- kernel/kprobes.c
+++ kernel/kprobes.c
@@ -2378,6 +2378,7 @@ static int __init init_kprobes(void)
 		init_test_probes();
 	return err;
 }
+postcore_initcall(init_kprobes);
 
 #ifdef CONFIG_DEBUG_FS
 static void report_probe(struct seq_file *pi, struct kprobe *p,
@@ -2703,5 +2704,3 @@ error:
 
 late_initcall(debugfs_kprobe_init);
 #endif /* CONFIG_DEBUG_FS */
-
-module_init(init_kprobes);
